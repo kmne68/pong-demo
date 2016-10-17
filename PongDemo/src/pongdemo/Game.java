6cbca3dde584bcfd15a1e5323c44e05e5138c5a4
Keith Emery <kmne68@gmail.com>
2016-10-17 13:19:28 -0500

    Add custom shapes, circle, round rectangle.

--- PongDemo/src/pongdemo/Game.java
+++ PongDemo/src/pongdemo/Game.java
@@ -8,6 +8,10 @@ package pongdemo;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
 import javax.swing.JComponent;
 
 /**
@@ -21,10 +25,17 @@ public class Game extends JComponent {
         
         Graphics2D g2 = (Graphics2D) g;
         
-        g2.setColor(Color.BLACK);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         
+        g2.setColor(Color.BLACK);
+        // fill background
         g2.fillRect(0, 0, getWidth(), getHeight());
-    }
-    
-    
+        
+        g2.setColor(Color.RED);
+        g2.fill(new Ellipse2D.Double(100, 100, 15, 15));
+        
+        g2.setColor(Color.BLUE);
+        g2.fill(new RoundRectangle2D.Double(200, 200, 100, 10, 20, 20));
+        
+    }    
 }
--- PongDemo/src/pongdemo/Main.java
+++ PongDemo/src/pongdemo/Main.java
@@ -37,6 +37,7 @@ public class Main extends JApplet {
     @Override
     public void init() {
         
+        setSize(600, 500);
         setLayout(new BorderLayout());
         
         add(new Game(), BorderLayout.CENTER);
