a4c72db74c89716b7c20ab623ffa6cd6fa0f964b
Keith Emery <kmne68@gmail.com>
2016-10-19 13:31:45 -0500

    Add double buffering

--- PongDemo/src/pongdemo/Game.java
+++ PongDemo/src/pongdemo/Game.java
@@ -12,6 +12,7 @@ import java.awt.RenderingHints;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
 import javax.swing.JComponent;
 
 /**
@@ -24,11 +25,17 @@ public class Game extends JComponent {
     private double speed = 10.0;
     private int xDirectionBall = 1;
     private int yDirectionBall = 1;
+    
+    private BufferedImage buffer;
 
     @Override
     protected void paintComponent(Graphics g) {
         
-        Graphics2D g2 = (Graphics2D) g;
+        if(buffer == null) {
+            buffer = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        }
+        
+        Graphics2D g2 = (Graphics2D) buffer.getGraphics();
         
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         
@@ -41,6 +48,8 @@ public class Game extends JComponent {
         
         g2.setColor(Color.BLUE);
         g2.fill(new RoundRectangle2D.Double(200, 200, 100, 10, 20, 20));
+        
+        g.drawImage(buffer, 0, 0, null);
         
     }   
     
@@ -69,4 +78,10 @@ public class Game extends JComponent {
         
         repaint(); // saves time vs calling paintComponent
     }
+
+    @Override
+    public void update(Graphics g) {
+        paint(g);
+    }
+    
 }
