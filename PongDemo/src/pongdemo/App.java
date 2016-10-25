69564834c95e4cfdb2722f416b67481860189048
Keith Emery <kmne68@gmail.com>
2016-10-25 13:39:51 -0500

    Add main method to run as app.

---
+++ PongDemo/src/pongdemo/App.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pongdemo;
+
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author kemery
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        
+        SwingUtilities.invokeLater(()->{ new MainFrame(); });
+        /* Traditional method
+        SwingUtilities.invokeLater(new Runnable() {
+            
+            public void run() {
+                new MainFrame();
+            }
+               
+        
+        });
+        
+        */ 
+    }
+    
+}
---
+++ PongDemo/src/pongdemo/MainFrame.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pongdemo;
+
+import java.awt.CardLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+/**
+ *
+ * @author kemery
+ */
+public class MainFrame extends JFrame implements ActionListener {
+    
+    private Timer timer;    // could go in game class as that is where drawing takes place
+    long startTime;
+    long relativeTime;
+    private Game game;
+    private StartPanel startPanel;
+    private CardLayout cards;
+    
+    
+    MainFrame() {
+        
+        super("Animation Demo");
+        
+cards = new CardLayout();
+        startPanel = new StartPanel();
+        game = new Game();
+        
+        startPanel.setListener(new StartPanelListener() {
+            
+            public void startGame() {
+                cards.show(MainFrame.this.getContentPane(), "game");
+            }            
+        });
+        
+        timer = new Timer(20, this);
+        timer.setInitialDelay(100);
+        
+        setSize(600, 500);
+        setLayout(cards);
+        
+        add(startPanel, "start");
+        add(game, "game");  
+        
+        timer.start();
+        
+        setSize(800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        game.update();
+    }
+    
+}
